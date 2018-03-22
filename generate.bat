97b8758adf5496074b2e75eaefd0fc4dddb9cae9
Ivan <john.matyash@gmail.com>
2018-03-23 01:23:21 +0200

    small refactoring

--- generate.bat
+++ generate.bat
@@ -1,1 +1,1 @@
-start /wait "nunit" "%cd%\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" %cd%\RestApiTest\bin\Debug\RestApiTest.dll && start "reportunit" "%cd%\packages\ReportUnit.1.2.1\tools\ReportUnit.exe" %cd%\TestResult.xml %cd%\TestResult.html
+start /wait "nunit" ".\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" .\RestApiTest\bin\Debug\RestApiTest.dll && start "reportunit" ".\packages\ReportUnit.1.2.1\tools\ReportUnit.exe" .\TestResult.xml .\TestResult.html
